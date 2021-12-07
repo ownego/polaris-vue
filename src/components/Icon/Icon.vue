7089a4eea580c247aaa45312831b6d5fb1dd0d9b
Quoc Cuong <quoccuong14012000@gmail.com>
2021-12-07 10:40:14 +0700

    Update: update component description for Icon component

--- src/components/Icon/Icon.vue
+++ src/components/Icon/Icon.vue
@@ -44,6 +44,14 @@ const COLORS_WITH_BACKDROPS: string[] = [
   'warning',
 ];
 
+/**
+ * <br/>
+ * <h4 class="desc">
+ * Icons are used to visually communicate core parts of the product and available actions.
+ * They can act as wayfinding tools to help merchants more easily understand
+ * where they are in the product, and common interaction patterns that are available.
+ * </h4>
+ */
 @Component
 export default class Icon extends Vue {
   /**
