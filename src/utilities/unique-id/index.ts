dd79774a421dc260dc516810181882ef6c536d53
Quoc Cuong <quoccuong14012000@gmail.com>
2021-12-17 12:14:32 +0700

    Fix: change method name globalIdGeneratorFactory to globalIdGenerator

--- src/utilities/unique-id/index.ts
+++ src/utilities/unique-id/index.ts
@@ -1,17 +1,17 @@
-export const globalIdGeneratorFactory = (prefix: string, index = 1): string => {
+export const globalIdGenerator = (prefix: string, index = 1): string => {
   const queryString = `#Polaris${prefix}${index}`;
   const useUniqueIdRef = document.querySelector(queryString);
   const newIndex = index + 1;
 
   if (!useUniqueIdRef) return `Polaris${prefix}${index}`;
 
-  return globalIdGeneratorFactory(prefix, newIndex);
+  return globalIdGenerator(prefix, newIndex);
 };
 
 export const useUniqueID = (prefix: string, overrideID: string): string => {
   if (overrideID) return overrideID;
 
-  return globalIdGeneratorFactory(prefix);
+  return globalIdGenerator(prefix);
 };
 
 export default {};
