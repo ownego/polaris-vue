50560f913e17f46f7eecc9915857075d0bec22ff
Quoc Cuong <quoccuong14012000@gmail.com>
2021-12-20 16:04:37 +0700

    Update: modify useUniqueId method

--- src/utilities/unique-id/index.ts
+++ src/utilities/unique-id/index.ts
@@ -2,10 +2,7 @@ export const generateId = (): number => Math.floor(10000 + Math.random() * 90000
 
 export const useUniqueId = (prefix: string, overrideId: string): string => {
   const stringId = `Polaris${prefix}`;
-
-  if (overrideId) return overrideId;
-
-  return stringId + generateId();
+  return overrideId || stringId + generateId();
 };
 
 export default {};
