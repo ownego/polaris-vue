28744f9617af9c3dc9801db378e53ada684c5300
oe.sonnh <sonnh.90@gmail.com>
2021-12-31 12:39:52 +0700

    Update: Label completed

---
+++ src/components/Label/Label.vue
@@ -0,0 +1,58 @@
+<template lang="pug">
+div(:class="className")
+  label(
+    :id="labelID",
+    :for="id",
+    :class="labelClassName",
+  )
+    slot
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component, Prop } from 'vue-property-decorator';
+import { classNames } from 'polaris-react/src/utilities/css';
+import styles from '@/classes/Label.json';
+
+export interface LabelProps {
+  /** A unique identifier for the label */
+  id: string;
+  /** Visually hide the label */
+  hidden?: boolean;
+  /** Visual required indicator for the label */
+  requiredIndicator?: boolean;
+}
+
+@Component
+export default class Label extends Vue {
+  @Prop({ type: String, required: true })
+  public id!: string;
+
+  @Prop({ type: Boolean })
+  public hidden?: boolean;
+
+  @Prop({ type: Boolean })
+  public requiredIndicator?: boolean;
+
+  get className() {
+    return classNames(
+      styles.Label,
+      this.hidden && styles.hidden,
+    );
+  }
+
+  get labelClassName() {
+    return classNames(
+      styles.Text,
+      this.requiredIndicator && styles.RequiredIndicator,
+    );
+  }
+
+  get labelID() {
+    return `${this.id}Label`;
+  }
+}
+</script>
+<style lang="scss">
+@import 'polaris-react/src/components/Label/Label.scss';
+</style>
