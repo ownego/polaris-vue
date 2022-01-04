5b0813d38b9201d421c0e55bdfd61c430051f369
Quoc Cuong <quoccuong14012000@gmail.com>
2022-01-04 11:32:20 +0700

    Update: reduce space between props

--- src/components/Checkbox/utils.ts
+++ src/components/Checkbox/utils.ts
@@ -1,46 +1,28 @@
 export interface CheckboxProps {
- /**
-   * Indicates the ID of the element that is controlled by the checkbox
-   */
+ /* Indicates the ID of the element that is controlled by the checkbox */
   ariaControl?: string;
 
-  /**
-   * Indicates the ID of the element that describes the checkbox
-   */
+  /* Indicates the ID of the element that describes the checkbox */
   ariaDescribedBy?: string;
 
-  /**
-   * Visually hide the label
-   */
+  /* Visually hide the label */
   labelHidden?: boolean;
 
-  /**
-   * Checkbox is selected. `indeterminate` shows a horizontal line in the checkbox
-   */
+  /* Checkbox is selected. `indeterminate` shows a horizontal line in the checkbox */
   checked?: boolean | 'indeterminate';
 
-  /**
-   * Disable input
-   */
+  /* Disable input */
   disabled?: boolean;
 
-  /**
-   * ID for form input
-   */
+  /* ID for form input */
   id?: string;
 
-  /**
-   * Name for form input
-   */
+  /* Name for form input */
   name?: string;
 
-  /**
-   * Value for form input
-   */
+  /* Value for form input */
   value?: string | boolean;
 
-  /**
-   * Display an error message
-   */
+  /* Display an error message */
   error?: Error | boolean;
 }
