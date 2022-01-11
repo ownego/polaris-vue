e216c4771e9206cc54cbfb13021e0b0935a319cd
Huong <tth17598@gmail.com>
2022-01-11 17:14:36 +0700

    init component

---
+++ src/components/ActionList/ActionList.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component
+export default class ActionList extends Vue {
+}
+</script>
+<style lang="scss">
+@import 'polaris-react/src/components/ActionList/ActionList.scss';
+</style>
---
+++ src/components/ActionList/components/Item/Item.vue
@@ -0,0 +1,11 @@
+<template lang="pug">
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import { Component } from 'vue-property-decorator';
+
+@Component
+export default class Item extends Vue {
+}
+</script>
