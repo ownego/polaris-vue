e0d53ae1f600afced222679cf8e5f634c09b41f9
Huong <tth17598@gmail.com>
2022-01-19 14:28:01 +0700

    Update: register portal locally

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-import PortalVue from 'portal-vue';
 import Demo from './Demo.vue';
 import PolarisVue from './polaris-vue';
 
@@ -8,7 +7,6 @@ Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
 Vue.use(PolarisVue);
-Vue.use(PortalVue);
 
 new Vue({
   router: new VueRouter(),
