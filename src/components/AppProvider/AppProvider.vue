080d164f6580e1ef39c99143263198c8e7915539
oe.sonnh <sonnh.90@gmail.com>
2022-02-16 16:46:34 +0700

    Complete component

--- src/components/AppProvider/AppProvider.vue
+++ src/components/AppProvider/AppProvider.vue
@@ -1,18 +1,29 @@
 <template lang="pug">
-div
+CustomProperties
   slot
   #PolarisPortalsContainer
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import { Component, Provide } from 'vue-property-decorator';
+import {
+  Component, Prop, Provide, Watch,
+} from 'vue-property-decorator';
 import { ScrollLockManager } from 'polaris-react/src/utilities/scroll-lock-manager/scroll-lock-manager';
 import { UniqueIdFactory, globalIdGeneratorFactory } from 'polaris-react/src/utilities/unique-id/unique-id-factory';
 import { PortalManager } from '@/utilities/portal-manager';
 import { FocusManager } from '@/utilities/focus-manager';
+import {
+  CustomProperties,
+  CustomPropertiesProps,
+  DEFAULT_COLOR_SCHEME,
+} from '../CustomProperties';
 
-@Component
+@Component({
+  components: {
+    CustomProperties,
+  },
+})
 export default class AppProvider extends Vue {
   @Provide() scrollLockManager = new ScrollLockManager();
 
@@ -21,6 +32,33 @@ export default class AppProvider extends Vue {
   @Provide() uniqueIdFactory = new UniqueIdFactory(globalIdGeneratorFactory);
 
   @Provide() focusManager = new FocusManager();
+
+  @Prop({ type: String })
+  colorScheme?: CustomPropertiesProps['colorScheme'];
+
+  @Watch('colorScheme')
+  onColorSchemeChange(newColorScheme: CustomPropertiesProps['colorScheme'], oldColorScheme: CustomPropertiesProps['colorScheme']) {
+    if (newColorScheme !== oldColorScheme) {
+      this.setBodyStyles();
+    }
+  }
+
+  created() {
+    if (document !== null) {
+      this.setBodyStyles();
+    }
+  }
+
+  setBodyStyles = () => {
+    // Inlining the following custom properties to maintain backward
+    // compatibility with the legacy ThemeProvider implementation.
+    document.body.setAttribute(
+      'p-color-scheme',
+      this.colorScheme || DEFAULT_COLOR_SCHEME,
+    );
+    document.body.style.backgroundColor = 'var(--p-background)';
+    document.body.style.color = 'var(--p-text)';
+  };
 }
 </script>
 
---
+++ src/components/CustomProperties/index.ts
@@ -0,0 +1,2 @@
+export { default as CustomProperties } from './CustomProperties.vue';
+export { DEFAULT_COLOR_SCHEME, STYLE_SHEET_ID, CustomPropertiesProps } from './utils';
