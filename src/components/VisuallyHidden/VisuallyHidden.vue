6406a68909936e568fcc22c8f608c9c7dc24d92f
Quoc Cuong <quoccuong14012000@gmail.com>
2022-02-23 14:44:55 +0700

    Update: add comment for prevent lint warning

--- src/components/VisuallyHidden/VisuallyHidden.vue
+++ src/components/VisuallyHidden/VisuallyHidden.vue
@@ -5,6 +5,7 @@ span(:class="styles.VisuallyHidden")
 </template>
 
 <script setup lang="ts">
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from '@/classes/VisuallyHidden.json';
 </script>
 
