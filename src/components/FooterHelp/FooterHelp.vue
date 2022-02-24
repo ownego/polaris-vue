b7209781ab4004b159f48d5935220831824a1264
Quoc Cuong <quoccuong14012000@gmail.com>
2022-02-24 16:32:51 +0700

    Update: Done on FooterHelp component

---
+++ src/components/FooterHelp/FooterHelp.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+div(:class="styles.FooterHelp")
+  div(:class="styles.Text")
+    slot
+</template>
+
+<script setup lang="ts">
+import styles from '@/classes/FooterHelp.json';
+</script>
+
+<style lang="scss">
+@import 'polaris-react/src/components/FooterHelp/FooterHelp.scss';
+</style>
