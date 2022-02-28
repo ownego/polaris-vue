311d49a9b6fc205c8cdf9ffb827f6c73da554500
Jane <tth17598@gmail.com>
2022-02-28 13:10:54 +0700

    WIP: Popover component

---
+++ src/components/Popover/utils.ts
@@ -0,0 +1,129 @@
+/* eslint-disable max-len */
+import type { PositionedOverlayProps } from '../PositionedOverlay/utils';
+import type { CustomPropertiesProps } from '../CustomProperties/utils';
+
+export enum PopoverCloseSource {
+  Click,
+  EscapeKeypress,
+  FocusOut,
+  ScrollOut,
+}
+
+export enum TransitionStatus {
+  Entering = 'entering',
+  Entered = 'entered',
+  Exiting = 'exiting',
+  Exited = 'exited',
+}
+
+export type PopoverAutofocusTarget = 'none' | 'first-node' | 'container';
+
+export function nodeContainsDescendant(
+  rootNode: HTMLElement,
+  descendant: HTMLElement,
+): boolean {
+  if (rootNode === descendant) {
+    return true;
+  }
+
+  let parent = descendant.parentNode;
+
+  while (parent != null) {
+    if (parent === rootNode) {
+      return true;
+    }
+    parent = parent.parentNode;
+  }
+
+  return false;
+}
+
+export function setActivatorAttributes(
+  activator: HTMLElement,
+  {
+    id,
+    active = false,
+    ariaHaspopup,
+    activatorDisabled = false,
+  }: {
+    id: string;
+    active: boolean;
+    ariaHaspopup: string;
+    activatorDisabled: boolean;
+  },
+) {
+  if (!activatorDisabled) {
+    // eslint-disable-next-line no-param-reassign
+    activator.tabIndex = activator.tabIndex || 0;
+  }
+
+  activator.setAttribute('aria-controls', id);
+  activator.setAttribute('aria-owns', id);
+  activator.setAttribute('aria-expanded', String(active));
+
+  if (ariaHaspopup != null) {
+    activator.setAttribute('aria-haspopup', String(ariaHaspopup));
+  }
+}
+
+export interface PopoverOverlayProps {
+  fullWidth?: boolean;
+  fullHeight?: boolean;
+  fluidContent?: boolean;
+  preferredPosition?: PositionedOverlayProps['preferredPosition'];
+  preferredAlignment?: PositionedOverlayProps['preferredAlignment'];
+  active: boolean;
+  id: string;
+  zIndexOverride?: number;
+  activator: HTMLElement;
+  preferInputActivator?: PositionedOverlayProps['preferInputActivator'];
+  sectioned?: boolean;
+  fixed?: boolean;
+  hideOnPrint?: boolean;
+  colorScheme?: CustomPropertiesProps['colorScheme'];
+  autofocusTarget?: PopoverAutofocusTarget;
+}
+
+export interface PopoverProps {
+  /** The preferred direction to open the popover */
+  preferredPosition?: PopoverOverlayProps['preferredPosition'];
+  /** The preferred alignment of the popover relative to its activator */
+  preferredAlignment?: PopoverOverlayProps['preferredAlignment'];
+  /** Show or hide the Popover */
+  active: boolean;
+  /**
+   * Use the activator's input element to calculate the Popover position
+   * @default true
+   */
+  preferInputActivator?: PopoverOverlayProps['preferInputActivator'];
+  /**
+   * The element type to wrap the activator with
+   * @default 'div'
+   */
+  activatorWrapper?: string;
+  /** Override on the default z-index of 400 */
+  zIndexOverride?: number;
+  /** Prevents focusing the activator or the next focusable element when the popover is deactivated */
+  preventFocusOnClose?: boolean;
+  /** Automatically add wrap content in a section */
+  sectioned?: boolean;
+  /** Allow popover to stretch to the full width of its activator */
+  fullWidth?: boolean;
+  /** Allow popover to stretch to fit content vertically */
+  fullHeight?: boolean;
+  /** Allow popover content to determine the overlay width and height */
+  fluidContent?: boolean;
+  /** Remains in a fixed position */
+  fixed?: boolean;
+  /** Used to illustrate the type of popover element */
+  ariaHaspopup?: string;
+  /** Allow the popover overlay to be hidden when printing */
+  hideOnPrint?: boolean;
+  /** Accepts a color scheme for the contents of the popover */
+  colorScheme?: PopoverOverlayProps['colorScheme'];
+  /**
+   * The preferred auto focus target defaulting to the popover container
+   * @default 'container'
+   */
+  autofocusTarget?: PopoverAutofocusTarget;
+}
