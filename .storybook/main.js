01c7372101bc997039c843128177927c07e53153
oe.sonnh <sonnh@ownego.com>
2022-03-03 15:07:11 +0700

    Minor fix export & uncomment storybook README pattern

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = {
   stories: [
     './stories/**/*.stories.mdx',
-    // '../src/**/README.stories.mdx',
+    '../src/**/README.stories.mdx',
   ],
   addons: [
     '@storybook/addon-links',
