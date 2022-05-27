6c14aa8c26e6dea80646a59e158f8f39941373ed
oe.sonnh <sonnh@ownego.com>
2022-05-27 15:28:02 +0700

    WIP: BulkActions components

---
+++ src/components/BulkActions/BulkActions.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+</template>
+
+<script setup lang="ts">
+</script>
+
+<style lang="scss">
+@import 'polaris/polaris-react/src/components/BulkActions/BulkActions.scss';
+</style>
---
+++ src/components/BulkActions/index.ts
@@ -0,0 +1,1 @@
+export { default as BulkActions } from './BulkActions.vue';
