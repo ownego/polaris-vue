83640896833864c077bcde3ef5e3a40f05c10eec
oe.sonnh <sonnh@ownego.com>
2022-06-24 11:56:30 +0700

    WIP: trying exmaple to fix bug, stuck at choicelist multiple values

---
+++ src/components/Filters/components/ConnectedFilterControl/components/Item/Item.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+div(
+  @blur="() => focused = false",
+  @focus="() => focused = true",
+  :class="className",
+)
+  slot
+</template>
+<script setup lang="ts">
+import { ref, computed } from 'vue';
+import { classNames } from 'polaris/polaris-react/src/utilities/css';
+import styles from '@/classes/Filters-ConnectedFilterControl.json';
+
+const focused = ref(false);
+
+const className = computed(() => classNames(
+  styles.Item,
+  focused.value && styles['Item-focused'],
+));
+</script>
