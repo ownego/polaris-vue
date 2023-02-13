5a6b0db021e0f873a4858e62d0c88f51c8ba09f1
oe.sonnh <sonnh@ownego.com>
2023-02-13 12:29:33 +0700

    Fix: interface import issue in vue 3, using interface directly in Collapsible component

--- src/components/Collapsible/utils.ts
+++
@@ -1,6 +0,0 @@
-export interface TransitionCollapsible {
-  /** Assign a transition duration to the collapsible animation. */
-  duration?: string;
-  /** Assign a transition timing function to the collapsible animation */
-  timingFunction?: string;
-}
