291fbe64547a8cc43d0b16d714266e9b3eb988ca
Quoc Cuong <quoccuong14012000@gmail.com>
2023-03-16 11:04:14 +0700

    Update: add vue-router for development stage to work with vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,19 @@
 import { createApp } from 'vue';
+import { createWebHistory, createRouter } from 'vue-router';
 import Demo from './Demo.vue';
 import PolarisVue from './polaris-vue';
 
+const routes = [
+  { path: '/', component: Demo },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-createApp(Demo as any).use(PolarisVue).mount('#app');
+createApp(Demo as any)
+  .use(PolarisVue)
+  .use(router)
+  .mount('#app');
