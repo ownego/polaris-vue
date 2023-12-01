2c20e307e6b2c2b809a7c1068917b0604bf7ef08
Quoc Cuong <quoccuong14012000@gmail.com>
2023-12-01 14:57:46 +0700

    Wip: working on type defination for UnstyledLink

---
+++ src/components/UnstyledLink/UnstyledLink.vue
@@ -0,0 +1,14 @@
+<template lang="pug">
+</template>
+
+<script setup lang="ts">
+import { unstyled } from '@/components/shared';
+import { LinkLikeComponentProps, useLink } from '@/utilities/link';
+
+
+// The script in the style guide that generates the Props Explorer data expects
+// that the interface defining the props is defined in this file, not imported
+// from elsewhere. This silly workaround ensures that the Props Explorer table
+// is generated correctly.
+export interface UnstyledLinkProps extends LinkLikeComponentProps {}
+</script>
