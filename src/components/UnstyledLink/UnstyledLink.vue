e509d730ee5788833312a9489ec2592dc083f96d
Quoc Cuong <quoccuong14012000@gmail.com>
2023-12-01 16:49:32 +0700

    Update: change from VueElemenConstrutor to type Component, done on UnstyledLink component

--- src/components/UnstyledLink/UnstyledLink.vue
+++ src/components/UnstyledLink/UnstyledLink.vue
@@ -1,14 +1,56 @@
 <template lang="pug">
+component(
+  v-if="LinkComponent",
+  :is="LinkComponent",
+  v-bind="{ ...unstyled.props, ...props }",
+)
+
+a(
+  v-else,
+  :target="target",
+  :href="url"
+  :rel="rel",
+  v-bind="{ ...linkProps.rest, ...unstyled.props }",
+)
 </template>
 
 <script setup lang="ts">
+import { computed } from 'vue';
+import useLink from '@/use/useLink';
 import { unstyled } from '@/components/shared';
-import { LinkLikeComponentProps, useLink } from '@/utilities/link';
+import { LinkLikeComponentProps } from '@/utilities/link';
 
-
-// The script in the style guide that generates the Props Explorer data expects
-// that the interface defining the props is defined in this file, not imported
-// from elsewhere. This silly workaround ensures that the Props Explorer table
-// is generated correctly.
 export interface UnstyledLinkProps extends LinkLikeComponentProps {}
+
+const props = defineProps<UnstyledLinkProps>();
+
+const LinkComponent = useLink();
+
+const linkProps = computed(() => {
+  const {
+    external,
+    url,
+    target: targetProp,
+    ...rest,
+  } = props;
+
+  return {
+    external,
+    url,
+    target: targetProp,
+    ...rest,
+  };
+})
+
+const target = computed(() => {
+  if (props.external) {
+    return '_blank';
+  } else {
+    return props.target ?? undefined;
+  }
+});
+
+const rel = computed(() => {  
+  return target.value === '_blank' ? 'noopener noreferrer' : undefined;
+});
 </script>
