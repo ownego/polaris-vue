0e943a15e95616500dad4f6b3a4bcde77b16e456
Quoc Cuong <quoccuong14012000@gmail.com>
2023-12-05 11:52:04 +0700

    Update: modify LinkLikeComponentProps

--- src/components/UnstyledLink/UnstyledLink.vue
+++ src/components/UnstyledLink/UnstyledLink.vue
@@ -2,7 +2,7 @@
 component(
   v-if="LinkComponent",
   :is="LinkComponent",
-  v-bind="{ ...unstyled.props, ...props }",
+  v-bind="{ ...attrs, ...unstyled.props, ...props }",
 )
 
 a(
@@ -10,47 +10,44 @@ a(
   :target="target",
   :href="url"
   :rel="rel",
-  v-bind="{ ...linkProps.rest, ...unstyled.props }",
+  v-bind="{ ...attrs, ...linkProps.rest, ...unstyled.props }",
 )
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
+import { useAttrs, computed } from 'vue';
 import useLink from '@/use/useLink';
 import { unstyled } from '@/components/shared';
-import { LinkLikeComponentProps } from '@/utilities/link';
+import type { LinkLikeComponentProps } from '@/utilities/link';
 
 export interface UnstyledLinkProps extends LinkLikeComponentProps {}
 
-const props = defineProps<UnstyledLinkProps>();
+const attrs = useAttrs();
 
 const LinkComponent = useLink();
+
+const props = defineProps<UnstyledLinkProps>()
 
 const linkProps = computed(() => {
   const {
     external,
     url,
-    target: targetProp,
-    ...rest,
+    target,
+    ...rest
   } = props;
 
   return {
     external,
     url,
-    target: targetProp,
-    ...rest,
+    rest,
   };
-})
+});
 
 const target = computed(() => {
-  if (props.external) {
-    return '_blank';
-  } else {
-    return props.target ?? undefined;
-  }
+  return props.target ? `target="${props.target}"` : undefined;
 });
 
 const rel = computed(() => {  
-  return target.value === '_blank' ? 'noopener noreferrer' : undefined;
+  return props.target === '_blank' ? 'noopener noreferrer' : undefined;
 });
 </script>
