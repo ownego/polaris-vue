613a3bb6dce6d8908d588c18772da29ea5e6b080
haild <haileduc1111@gmail.com>
2023-12-21 14:10:48 +0700

    Update: docs preview

---
+++ docs/components/BlockStack/PreviewPlaceholder.vue
@@ -0,0 +1,46 @@
+<template>
+<div
+  :style="wrapperStyle"
+>
+  <InlineStack align="center">
+    <div
+      style="color: var(--p-color-text-info-on-bg-fill)"
+    >
+      <Text
+        as="h2"
+        variant="bodyMd"
+        fontWeight="regular"
+        tone="text-inverse"
+      >
+        {{ label }}
+      </Text>
+    </div>
+  </InlineStack>
+</div>
+</template>
+
+<script setup lang="ts">
+interface PlaceholderProps {
+  height?: string;
+  width?: string;
+  label?: string;
+  showBorder?: boolean;
+}
+
+const props = withDefaults(defineProps<PlaceholderProps>(), {
+  height: 'auto',
+  width: 'auto',
+  label: '',
+  showBorder: false,
+});
+
+const wrapperStyle = {
+  background: 'var(--p-color-text-info)',
+  padding: '14px var(--p-space-200)',
+  height: props.height,
+  width: props.width,
+  borderBlockEnd: props.showBorder
+    ? '1px dashed var(--p-color-bg-surface-success)'
+    : 'none',
+};
+</script>
