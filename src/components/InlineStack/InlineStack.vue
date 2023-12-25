28024fda798bc73e9c26ea14d26766209eebacf0
haild <haileduc1111@gmail.com>
2023-12-25 11:49:41 +0700

    Update : computed for styles, define slots

--- src/components/InlineStack/InlineStack.vue
+++ src/components/InlineStack/InlineStack.vue
@@ -8,8 +8,16 @@ div(
 
 <script setup lang="ts">
 import { computed, useCssModule } from 'vue';
+import type { VueNode } from '@/utilities/types';
 import { getResponsiveProps, classNames } from '@/utilities/css';
 import type { InlineStackProps } from './types';
+
+export type InlineStackSlots = {
+  /** Inner content of the box */
+  default: (_: VueNode) => any;
+}
+
+defineSlots<InlineStackSlots>();
 
 const props = withDefaults(defineProps<InlineStackProps>(), {
   wrap: true,
@@ -17,12 +25,14 @@ const props = withDefaults(defineProps<InlineStackProps>(), {
 
 const styles = useCssModule();
 
-const style = {
-  '--pc-inline-stack-align': props.align,
-  '--pc-inline-stack-block-align': props.blockAlign,
-  '--pc-inline-stack-wrap': props.wrap ? 'wrap' : 'nowrap',
-  ...getResponsiveProps('inline-stack', 'gap', 'space', props.gap),
-};
+const style = computed(() => {
+  return {
+    '--pc-inline-stack-align': props.align,
+    '--pc-inline-stack-block-align': props.blockAlign,
+    '--pc-inline-stack-wrap': props.wrap ? 'wrap' : 'nowrap',
+    ...getResponsiveProps('inline-stack', 'gap', 'space', props.gap),
+  }
+});
 
 const className = computed(() => {
   return classNames(
@@ -30,6 +40,7 @@ const className = computed(() => {
   );
 });
 </script>
+
 <style lang="scss" module>
 @import '@polaris/components/InlineStack/InlineStack.scss'
 </style>
