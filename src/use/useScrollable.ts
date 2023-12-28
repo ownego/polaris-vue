5601b9d2adf19436e1980b80e11da44632c9b917
Quoc Cuong <quoccuong14012000@gmail.com>
2023-12-28 12:15:17 +0700

    Update: remove usScrollable.ts

--- src/use/useScrollable.ts
+++
@@ -1,9 +0,0 @@
-import { inject } from 'vue';
-
-type ScrollToPositionFn = (scrollY: number) => void;
-
-export default function useScrollable() {
-  const context = inject<ScrollToPositionFn | undefined>('scrollable');
-
-  return context;
-}
