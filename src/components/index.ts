43f9d65aee399169bd834600e4f3297d427605c8
Quoc Cuong <quoccuong14012000@gmail.com>
2023-12-28 10:42:41 +0700

    Update: remove unnecessary code

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export * from './AppProvider';
 export * from './Box';
 export * from './Layout';
-export * from './Popover';
 export * from './Portal';
 export * from './Text';
 export * from './UnstyledLink';
