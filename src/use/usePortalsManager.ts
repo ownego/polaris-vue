36eada2b109c78bee0a373c068e86dd353251d13
Quoc Cuong <quoccuong14012000@gmail.com>
2023-12-28 09:48:33 +0700

    Wip: done on ScrollTo, working on Scrollable

--- src/use/usePortalsManager.ts
+++ src/use/usePortalsManager.ts
@@ -1,6 +1,19 @@
-import { type Ref, inject } from 'vue';
+import { inject } from 'vue';
+
+type PortalsContainerElement = HTMLElement | null;
+
+export interface PortalsManager {
+  container: PortalsContainerElement;
+}
 
 export default function usePortalsManager() {
-  const context = inject('portals-manager') as Ref<HTMLElement | null>;
+  const context = inject<PortalsManager | undefined>('portals-manager', undefined);
+
+  if (!context) {
+    throw new Error(
+      'No portals manager was provided. Your application must be wrapped in an <AppProvider> component. See https://polaris.shopify.com/components/app-provider for implementation instructions.',
+    ); 
+  }
+
   return context;
 }
