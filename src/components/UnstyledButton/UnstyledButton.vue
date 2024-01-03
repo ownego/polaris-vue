84811adccef3edd68143f7efe7264f0d7e115339
haild <haileduc1111@gmail.com>
2024-01-03 09:35:34 +0700

    Update: using rest instead of attrs

--- src/components/UnstyledButton/UnstyledButton.vue
+++ src/components/UnstyledButton/UnstyledButton.vue
@@ -3,7 +3,7 @@ a(v-if="url && disabled", v-bind="commonProps")
   slot
 UnstyledLink(
   v-else-if="url && !disabled",
-  v-bind="{ ...interactiveProps, ...$attrs }",
+  v-bind="{ ...interactiveProps, ...rest }",
   :url="url",
   :external="external",
   :download="download",
@@ -14,7 +14,7 @@ UnstyledLink(
   slot
 button(
   v-else,
-  v-bind="{ ...interactiveProps, ...$attrs }",
+  v-bind="{ ...interactiveProps, ...rest }",
   :aria-disabled="disabled",
   :disabled="disabled",
   :type="submit ? 'submit' : 'button'",
@@ -77,6 +77,27 @@ export type UnstyledButtonProps = {
 const props = defineProps<UnstyledButtonProps>();
 
 const attrs = useAttrs();
+
+const {
+  className,
+  id,
+  url,
+  external,
+  download,
+  target,
+  submit,
+  disabled,
+  loading,
+  pressed,
+  accessibilityLabel,
+  role,
+  ariaControls,
+  ariaExpanded,
+  ariaDescribedBy,
+  ariaChecked,
+  ...rest
+}: any = attrs;
+
 
 const getEventList = (events: string[]) => {
   const eventBindings: Record<string, any> = { mouseup: handleMouseUpByBlurring };
