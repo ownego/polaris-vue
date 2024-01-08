4649f79584a58282c4f678ea26a89e7a0d822ddf
haild <haileduc1111@gmail.com>
2024-01-08 14:25:11 +0700

    Update: Lable and Inline Err

---
+++ src/components/InlineError/InlineError.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+div(
+  v-if="message",
+  :class="styles.InlineError",
+  :id="errorTextId(fieldID)",
+)
+  div(:class="styles.Icon")
+    Icon(
+      :source="CircleAlertMajor",
+    )
+  span {{ message }}
+</template>
+
+<script setup lang="ts">
+import { useCssModule } from 'vue';
+import { Icon } from '@/components';
+import CircleAlertMajor from '@icons/CircleAlertMajor.svg';
+
+interface InlineErrorProps {
+  /** Content briefly explaining how to resolve the invalid form field input. */
+  message: string;
+  /** Unique identifier of the invalid form field that the message describes */
+  fieldID: string;
+}
+
+const styles = useCssModule();
+
+defineProps<InlineErrorProps>();
+
+function errorTextId(id: string): string {
+  return `${id}Error`;
+}
+</script>
+
+<style lang="scss" module>
+@import '@polaris/components/InlineError/InlineError.scss';
+</style>
---
+++ src/components/Labelled/Labelled.vue
@@ -0,0 +1,42 @@
+<template lang="pug">
+</template>
+
+<script setup lang="ts">
+import { computed, useCssModule } from 'vue';
+import { classNames } from '@/utilities/css';
+import type { Action } from '@/utilities/types';
+import type { LabelProps } from '@/components/Label/Label.vue';
+
+interface LabelledProps {
+  /** A unique identifier for the label */
+  id: LabelProps['id'];
+  /** Error to display beneath the label */
+  error?: Error | boolean;
+  /** An action */
+  action?: Action;
+  /** Visually hide the label */
+  labelHidden?: boolean;
+  /** Visual required indicator for the label */
+  requiredIndicator?: boolean;
+  /** Labels signify a disabled control */
+  disabled?: boolean;
+  /** Labels signify a readOnly control */
+  readOnly?: boolean;
+}
+
+const styles = useCssModule();
+
+const props = defineProps<LabelledProps>();
+
+const className = computed(() => {
+  return classNames(
+    props.labelHidden && styles.hidden,
+    props.disabled && styles.disabled,
+    props.readOnly && styles.readOnly,
+  )
+});
+</script>
+
+<style module lang="scss">
+@import '@polaris/components/Labelled/Labelled.scss';
+</style>
