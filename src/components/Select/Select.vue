e6fb83fda9019b3e731b43466a1f7f7b2abdf32b
oe.sonnh <sonnh@ownego.com>
2024-01-13 15:09:37 +0700

    WIP: select component computed

--- src/components/Select/Select.vue
+++ src/components/Select/Select.vue
@@ -3,10 +3,75 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, getCurrentInstance, ref } from 'vue';
+import { classNames, variationName } from '@/utilities/css';
+import { Labelled, Box, Icon, Text } from '@/components';
+import { helpTextID } from '@/components/Labelled/utils';
+import { useToggle } from '@/use/useToggle';
+import { useHasSlot } from '@/use/useHasSlot';
+import type { SelectProps, SelectSlots, SelectEvents } from './types';
 
-interface Props {
+import SelectMinor from '@icons/SelectMinor.svg';
+import styles from '@polaris/components/Select/Select.module.scss';
+
+const PLACEHOLDER_VALUE = '';
+
+const props = defineProps<SelectProps>();
+const slots = defineSlots<SelectSlots>();
+const emits = defineEmits<SelectEvents>();
+
+const model = defineModel<string>();
+
+const {
+  value: focused,
+  toggle: toggleFocused,
+} = useToggle(false);
+const { hasSlot } = useHasSlot();
+
+const uniqId = getCurrentInstance()?.uid;
+
+const id = computed(() => props.id || uniqId);
+const labelHidden = computed(() => props.labelInline ? true : props.labelHidden);
+
+const className = computed(() => (classNames(
+  styles.Select,
+  props.error && styles.error,
+  props.tone && styles[variationName('tone', props.tone)],
+  props.disabled && styles.disabled,
+)));
+
+const hasHelpText = computed(() => hasSlot(slots.helpText) || props.helpText);
+
+const ariaDescribedBy = computed(() => {
+  const describedBy = [];
+
+  if (hasHelpText.value) {
+    describedBy.push(helpTextID(`${id.value}`));
+  }
+
+  if (props.error) {
+    describedBy.push(`${id.value}Error`);
+  }
+
+  return describedBy.length ? describedBy.join(' ') : undefined;
+});
+
+const handleFocus = (e: FocusEvent) => {
+  toggleFocused();
+  emits('focus', e);
 }
 
-const props = defineProps<Props>();
+const handleBlur = (e: FocusEvent) => {
+  toggleFocused();
+  emits('blur', e);
+}
+
+const handleChange = (e: Event) => {
+  const target = e.target as HTMLSelectElement;
+  model.value = target.value;
+  emits('change', target.value, `${id.value}`);
+}
+
+
+
 </script>
---
+++ src/components/Select/types.ts
@@ -0,0 +1,81 @@
+import type { VNode } from 'vue';
+import type { Error, VueNode } from '@/utilities/types';
+import type { LabelledProps } from '@/components/Labelled/types';
+
+export interface StrictOption {
+  /** Machine value of the option; this is the value passed to `onChange` */
+  value: string;
+  /** Human-readable text for the option */
+  label: string;
+  /** Option will be visible, but not selectable */
+  disabled?: boolean;
+  /** Element to display to the left of the option label. Does not show in the dropdown. */
+  prefix?: VueNode;
+  /** Unique key applied to the option element. Defaults to option value prop when undefined. */
+  key?: string;
+}
+
+export interface HideableStrictOption extends StrictOption {
+  hidden?: boolean;
+}
+
+export interface StrictGroup {
+  /** Title for the group */
+  title: string;
+  /** List of options */
+  options: StrictOption[];
+}
+
+export type SelectOption = string | StrictOption;
+
+export interface SelectGroup {
+  title: string;
+  options: SelectOption[];
+}
+
+export type SelectProps = {
+  /** List of options or option groups to choose from */
+  options?: (SelectOption | SelectGroup)[];
+  /** Label for the select */
+  label?: string;
+  /** Adds an action to the label */
+  labelAction?: LabelledProps['action'];
+  /** Visually hide the label */
+  labelHidden?: boolean;
+  /** Show the label to the left of the value, inside the control */
+  labelInline?: boolean;
+  /** Disable input */
+  disabled?: boolean;
+  /** Additional text to aide in use */
+  helpText?: string;
+  /** Example text to display as placeholder */
+  placeholder?: string;
+  /** ID for form input */
+  id?: string;
+  /** Name for form input */
+  name?: string;
+  /** Value for form input */
+  value?: string;
+  /** Display an error state */
+  error?: Error | boolean;
+  /** Visual required indicator, add an asterisk to label */
+  requiredIndicator?: boolean;
+  /** Indicates the tone of the select */
+  tone?: 'magic';
+}
+
+export type SelectSlots = {
+  /** Label for the select */
+  label: (_?: VueNode) => VNode[];
+  /** Additional text to aide in use */
+  helpText: (_?: VueNode) => VNode[];
+}
+
+export type SelectEvents = {
+  /** Callback when selection is changed */
+  change: [selected: string, id: string];
+  /** Callback when select is focused */
+  focus: [event: FocusEvent];
+  /** Callback when focus is removed */
+  blur: [event: FocusEvent];
+}
