c5f7572a6154bd62a0f88828f5c7e46c8e048dc8
oe.sonnh <sonnh@ownego.com>
2024-01-24 14:24:42 +0700

    Fix: tab indent HexValue example

--- docs/components/ColorPicker/HexValue.vue
+++ docs/components/ColorPicker/HexValue.vue
@@ -1,9 +1,9 @@
 <template>
-  <ColorPicker v-model="color" output="hex" />
-  <p :style="{ marginTop: '1rem' }">Color value: {{ color }}</p>
-  </template>
+<ColorPicker v-model="color" output="hex" />
+<p :style="{ marginTop: '1rem' }">Color value: {{ color }}</p>
+</template>
 
-  <script setup lang="ts">
-  import { ref } from 'vue';
-  const color = ref('#3355aa');
-  </script>
+<script setup lang="ts">
+import { ref } from 'vue';
+const color = ref('#3355aa');
+</script>
