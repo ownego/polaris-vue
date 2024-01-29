eb7a38c6dc75799f6400782e3e958297d739c129
haild <haileduc1111@gmail.com>
2024-01-29 09:57:10 +0700

    Update: CheckableButton comp done

---
+++ src/components/CheckableButton/CheckableButton.vue
@@ -0,0 +1,52 @@
+<template lang="pug">
+div(:class="className", @click="$emit('toggle-all')")
+  div(:class="styles.Checkbox")
+    Checkbox(
+      v-model="selected",
+      ref="checkBoxRef",
+      labelHidden,
+      :label="accessibilityLabel",
+      :disabled="disabled",
+      @change="$emit('toggle-all')",
+    )
+  span(:class="styles.Label") {{ label }}
+</template>
+
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import { classNames } from '@/utilities/css';
+import { Checkbox } from '@/components';
+import styles from '@polaris/components/CheckableButton/CheckableButton.module.scss';
+
+export interface CheckboxHandles {
+  focus(): void;
+}
+
+interface CheckableButtonProps {
+  accessibilityLabel?: string;
+  label?: string;
+  selected?: boolean | 'indeterminate';
+  selectMode?: boolean;
+  smallScreen?: boolean;
+  plain?: boolean;
+  measuring?: boolean;
+  disabled?: boolean;
+}
+
+withDefaults(defineProps<CheckableButtonProps>(), {
+  label: '',
+});
+const emits = defineEmits<{
+  'toggle-all': [];
+}>();
+
+const checkBoxRef = ref<CheckboxHandles | null>(null);
+
+const className = computed(() => classNames(styles.CheckableButton));
+
+function focus() {
+  checkBoxRef.value?.focus();
+}
+
+defineExpose({ focus });
+</script>
