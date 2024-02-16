a8bc193576ae11738f417548a4322aec7a06da5b
haild <haileduc1111@gmail.com>
2024-02-16 09:33:13 +0700

    Update: ScrollLock comp

---
+++ src/components/ScrollLock/ScrollLock.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { onMounted, onUnmounted } from 'vue';
+import useScrollLockManager from '@/use/useScrollLockManager';
+
+const scrollLockManager = useScrollLockManager();
+
+onMounted(() => {
+  scrollLockManager.registerScrollLock();
+});
+
+onUnmounted(() => {
+  scrollLockManager.unregisterScrollLock();
+});
+</script>
+
+<style lang="scss">
+@import '@polaris/components/ScrollLock/ScrollLock.module.scss';
+</style>
