915a478e2125249f78289973d8543e91a2d11228
Quoc Cuong <quoccuong14012000@gmail.com>
2024-02-22 16:58:30 +0700

    Wip: done on Dialog component, clone from v1

--- src/components/Modal/components/Dialog/Dialog.vue
+++ src/components/Modal/components/Dialog/Dialog.vue
@@ -1,20 +1,38 @@
 <template lang="pug">
+div(
+  :class="styles.Container",
+  data-polaris-layer,
+  data-polaris-overlay,
+  ref="dialogNode",
+)
+  TrapFocus
+    div(
+      role="dialog",
+      aria-modal,
+      :aria-labelledby="labelledBy",
+      tabindex="-1",
+      :class="styles.Dialog",
+    )
+      div(:class="classes")
+        KeypressListener(
+          :key-code="Key.Escape",
+          key-event="keydown",
+          :handler="handleKeyDown",
+        )
+        KeypressListener(:keyCode="Key.Escape", :handler="handleKeyUp")
+        slot
+
 </template>
 
 <script setup lang="ts">
-import { inject, ref } from 'vue';
-import useTheme from '@/use/useTheme';
-import { focusFirstFocusableNode } from '@/utilities/focus';
+import { computed, onBeforeUnmount, onMounted, ref } from 'vue';
 import { classNames, variationName } from '@/utilities/css';
-import {
-  KeypressListener,
-  Text,
-  TrapFocus,
-} from '@/components';
 import styles from '@polaris/components/Modal/components/Dialog/Dialog.module.scss';
+import { focusFirstFocusableNode } from '@/utilities/focus';
+import { KeypressListener, TrapFocus } from '@/components';
 import { Key } from '@/utilities/types';
 import type { ModalSize } from '../utils';
-
+ 
 export interface DialogProps {
   labelledBy?: string;
   instant?: boolean;
@@ -24,7 +42,46 @@ export interface DialogProps {
   hasToasts?: boolean;
 }
 
-const theme = useTheme();
+const props = defineProps<DialogProps>();
 
-const containerNode = ref<HTMLDivElement | null>(null);
+const emit = defineEmits<{
+  (event: 'close'): void;
+  (event: 'closing', value: boolean): void;
+  (event: 'entered'): void;
+  (event: 'exited'): void;
+}>();
+
+const dialogNode = ref<HTMLElement | null>(null);
+const closing = ref(false);
+
+const classes = computed(() => {
+  return classNames(
+    styles.Modal,
+    props.size && styles[variationName('size', props.size)],
+    props.limitHeight && styles.limitHeight,
+  );
+});
+
+onMounted(() => {
+  if (dialogNode.value && dialogNode.value.contains(document.activeElement)) {
+    focusFirstFocusableNode(dialogNode.value);
+  }
+
+  emit('entered');
+});
+
+onBeforeUnmount(() => {
+  emit('exited');
+});
+
+const handleKeyDown = () => {
+  closing.value = true;
+  emit('closing', true);
+};
+
+const handleKeyUp = () => {
+  closing.value = false;
+  emit('closing', false);
+  emit('close');
+};
 </script>
