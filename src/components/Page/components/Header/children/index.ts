70a97708d697ee3c32468109fcdc48f4b8e95e0a
miph1309 <hami9696@gmail.com>
2024-02-29 09:58:31 +0700

    Update: page title

--- src/components/Page/components/Header/children/index.ts
+++ src/components/Page/components/Header/children/index.ts
@@ -1,3 +1,4 @@
 export { default as PrimaryAction } from './PrimaryAction.vue';
 export { default as ActionMenu } from './ActionMenu.vue';
 export { default as Pagination } from './Pagination.vue';
+export { default as PageTitle } from './PageTitle.vue';
